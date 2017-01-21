9cfbb747fb59f55a83bfaf04ca43f68ba26c107d
Rokudoku <Rokudoku@users.noreply.github.com>
2017-01-21 23:18:05 +1100

    Small spacing cleanup.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@
 
 from cx_Freeze import setup, Executable
 
-setup(name="Date Copy Tool",
-      options={"build_exe": {"packages":["tkinter"]}}
-      version="1.0.0",
-      description="Tool for copying dates to clipboard",
+setup(name = "Date Copy Tool",
+      options = {"build_exe": {"packages":["tkinter"]}}
+      version = "1.0.0",
+      description = "Tool for copying dates to clipboard",
       executables = [Executable("date_copy_tool.py")])
