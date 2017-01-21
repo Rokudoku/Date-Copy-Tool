128d335345e78c95483486e0535e3b6b74225955
Rokudoku <Rokudoku@users.noreply.github.com>
2017-01-21 23:45:31 +1100

    Attempted bugfix is unoptimal.
    Results in >1600 files build folder. Will look for other solution.

--- setup.py
+++ setup.py
@@ -3,19 +3,15 @@
 # and
 # https://www.youtube.com/watch?v=HosXxXE24hA
 
-# I also had an error - KeyError: 'TCL_LIBRARY' which I found the fix for:
-# http://stackoverflow.com/questions/34939356/python-pygame-exe-build-with-cx-freeze-tcl-library-error
-
 from cx_Freeze import setup, Executable
-import os
-
-os.environ['TCL_LIBRARY'] = "C:\\Users\\Jerome\\AppData\\Local\\Programs" \
-                            "\\Python\\Python35\\tcl\\tcl8.6"
-os.environ['TK_LIBRARY'] = "C:\\Users\\Jerome\\AppData\\Local\\Programs" \
-                           "\\Python\\Python35\\tcl\\tk8.6"
 
 setup(name = "Date Copy Tool",
       options = {"build_exe": {"packages":["tkinter"]}},
       version = "1.0.0",
       description = "Tool for copying dates to clipboard",
       executables = [Executable("date_copy_tool.py")])
+
+
+# First attempt resolted in KeyError: 'TCL_Library'.
+# The attempted bugfix to force the paths to point to my tcl and tk folders
+# resulted in >1600 file large build folder...
