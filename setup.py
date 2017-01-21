f8960193d709f4d954c154f118913d92f50037c2
Rokudoku <Rokudoku@users.noreply.github.com>
2017-01-21 23:16:46 +1100

    Added 'options' in setup.
    Will hopefully make the freeze tkinter friendly.

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-# This setup was made by following along with a youtube tutorial by sentdex:
-# (https://www.youtube.com/watch?v=GSoOwSqTSrs)
+# This setup was made by following along with youtube tutorials by sentdex:
+# https://www.youtube.com/watch?v=GSoOwSqTSrs
+# and
+# https://www.youtube.com/watch?v=HosXxXE24hA
 
 from cx_Freeze import setup, Executable
 
 setup(name="Date Copy Tool",
+      options={"build_exe": {"packages":["tkinter"]}}
       version="1.0.0",
       description="Tool for copying dates to clipboard",
       executables = [Executable("date_copy_tool.py")])
