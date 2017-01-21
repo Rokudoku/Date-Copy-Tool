c9f5cc6b7e173f0e94aae057a140a376cbf0902f
Rokudoku <Rokudoku@users.noreply.github.com>
2017-01-21 23:19:12 +1100

    Forgot comma.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from cx_Freeze import setup, Executable
 
 setup(name = "Date Copy Tool",
-      options = {"build_exe": {"packages":["tkinter"]}}
+      options = {"build_exe": {"packages":["tkinter"]}},
       version = "1.0.0",
       description = "Tool for copying dates to clipboard",
       executables = [Executable("date_copy_tool.py")])
