2aa6991f1aa28ab18c3fb6d7c995661e932cf894
Rokudoku <Rokudoku@users.noreply.github.com>
2017-01-23 22:12:15 +1100

    Added note on use and changed name.

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@
 # and
 # https://www.youtube.com/watch?v=HosXxXE24hA
 
+### NOTE: ONLY WORKS WITH PYTHON3.4 (tried 3.5 and 3.6)
+### created build by opening the location in cmd and typing
+### "python34 setup.py build" (after making a python34.exe and setting a PATH
+### to the folder)
+
 import sys
 from cx_Freeze import setup, Executable
 
@@ -10,7 +15,7 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-setup(name = "Date Copy Tool",
+setup(name = "Date-Copy-Tool",
       options = {"build_exe": {"packages":["tkinter"]}},
       version = "1.0.0",
       description = "Tool for copying dates to clipboard",
