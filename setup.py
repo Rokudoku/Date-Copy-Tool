7bd74a85a9b0ec4f18e346d5c15a25930a5d30e3
Rokudoku <Rokudoku@users.noreply.github.com>
2017-01-23 20:39:06 +1100

    Added base.

--- setup.py
+++ setup.py
@@ -5,11 +5,15 @@
 
 from cx_Freeze import setup, Executable
 
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
 setup(name = "Date Copy Tool",
       options = {"build_exe": {"packages":["tkinter"]}},
       version = "1.0.0",
       description = "Tool for copying dates to clipboard",
-      executables = [Executable("date_copy_tool.py")])
+      executables = [Executable("date_copy_tool.py", base=base)])
 
 
 # First attempt resolted in KeyError: 'TCL_Library'.
